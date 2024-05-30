ceb36444c76759334c066739fdd087f9bfe5050b
Troels <troelsdankert@gmail.com>
2024-05-30 11:51:35 +0200

    endpoint

--- modules/settings.js
+++ modules/settings.js
@@ -1,4 +1,4 @@
 export const apikey =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xenFpdnVreXZ1c3lxeWRqa2htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTE2NjUsImV4cCI6MjAyNjMyNzY2NX0.UUJvo-1OSO7KAM0yHfVJoBL6EyiWAayYJqfyE0ccqN4";
 export const endpoint =
-  "https://nqzqivukyvusyqydjkhm.supabase.co";
+  "https://nqzqivukyvusyqydjkhm.supabase.co/rest/v1/recipes";
